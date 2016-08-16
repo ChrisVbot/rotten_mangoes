8183d057f0cb19922b150a13e31c1ee8a3f0a0f2
ChrisVbot <chris.veillette@gmail.com>
2016-08-16 23:49:05 +0000

    admin features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      if 
+        user.admin
+        session[:admin_id] = user.id
+      end
       session[:user_id] = user.id
       redirect_to movies_path, notice: "Welcome back, #{user.firstname}"
     else
