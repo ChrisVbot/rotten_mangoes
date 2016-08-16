97a22aca5fa1ebf566a81d7367ebc524bdfe8475
ChrisVbot <chris.veillette@gmail.com>
2016-08-16 19:44:56 +0000

    image upload implemented

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,27 +23,28 @@ class MoviesController < ApplicationController
     else
       render :new
     end
+  end
 
-    def update
-      @movie = Movie.find(params[:id])
-      if @movie.update_attributes(movie_params)
-        redirect_to movies_path(@movie)
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @movie = Movie.find(params[:id])
-      @movie.destroy
-      redirect_to movies_path
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update_attributes(movie_params)
+      redirect_to movie_path(@movie), notice: "#{@movie.title} updated successfully!"
+    else
+      render :edit
     end
   end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+  
 
   protected
   def movie_params
     params.require(:movie).permit(
-      :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
+      :title, :release_date, :director, :runtime_in_minutes, :description, :image, :remove_image, :remote_image_url
       )
   end
 end
